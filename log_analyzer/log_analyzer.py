3d33ff49f0cc99c6d3f49c4832992fce8b0956f1
John Evans <scribhneior@gmail.com>
2020-05-27 07:25:08 -0400

    Replaced with 'with' statement

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -1,22 +1,20 @@
 #! python3
 import re
 
-text = open('log_analyzer/original.log', 'r')
 login_regex = re.compile(
 r'''username=[a-zA-Z0-9 !@#$%^&*./,]*&password=[a-zA-Z0-9 !@#$%^&*./,]*\s''')
 
 credentials_list = []
+with open('log_analyzer/original.log', 'r') as text:
+    for line in text:
+        for user in login_regex.findall(line):
+            user = user.strip()
+            user_id, password = user.split('&')
 
-for line in text:
-    for user in login_regex.findall(line):
-        user = user.strip()
-        user_id, password = user.split('&')
+            user_title, username = user_id.split('=')
+            pass_title, passphrase = password.split('=')
 
-        user_title, username = user_id.split('=')
-        pass_title, passphrase = password.split('=')
-
-        credentials_list.append('username:{}, password:{}'.format(
-            username,passphrase))
-text.close()
+            credentials_list.append('username:{}, password:{}'.format(
+                username,passphrase))
 
 print(credentials_list)
